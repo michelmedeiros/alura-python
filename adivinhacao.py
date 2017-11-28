68feb65b2d091f21b2b9ce999a973546771ee1fb
Michel Medeiros <michel.tds@gmail.com>
2017-11-27 22:06:57 -0200

    Inicio do projeto

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,15 @@
-print('Bem vindo ao jogo de adivinhação')
+print("        P  /_\  P                              ")
+print("       /_\_|_|_/_\                             ")
+print("   n_n | ||. .|| | n_n         Bem vindo ao    ")
+print("   |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação!")
+print("  |' '  |  |_|  |'  ' |                        ")
+print("  |_____| ' _ ' |_____|                        ")
+print("        \__|_|__/                              ")
+
+numero_secreto = 43
+chute = int(input("Digite o seu número: "))
+print("Você digitou: ", chute)
+if(chute == numero_secreto):
+    print("Você acertou")
+else:
+    print("Você errou")
